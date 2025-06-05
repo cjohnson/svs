35a195a19239b4b4fb15fd2fea3cde86247cf97d
cjohnson <collin.g.johnson@gmail.com>
2025-06-05 01:01:07 -0400

    Use __Assignment instead of __Equals

--- src/lexical_token/OperatorLexicalToken.cpp
+++ src/lexical_token/OperatorLexicalToken.cpp
@@ -13,19 +13,19 @@ struct OperatorLookupTableRow
 static const std::vector<OperatorLookupTableRow> operator_lookup_table
 {
     // Assignment Operators
-    { "=", "=", svs::OperatorLexicalToken::Type::Equals, "Equals" },
-    { "+=", "\\+=", svs::OperatorLexicalToken::Type::PlusEquals, "PlusEquals" },
-    { "-=", "-=", svs::OperatorLexicalToken::Type::MinusEquals, "MinusEquals" },
-    { "*=", "\\*=", svs::OperatorLexicalToken::Type::TimesEquals, "TimesEquals" },
-    { "/=", "/=", svs::OperatorLexicalToken::Type::DivideEquals, "DivideEquals" },
-    { "%=", "%=", svs::OperatorLexicalToken::Type::ModuloEquals, "ModuloEquals" },
-    { "&=", "&=", svs::OperatorLexicalToken::Type::BitwiseAndEquals, "BitwiseAndEquals" },
-    { "|=", "\\|=", svs::OperatorLexicalToken::Type::BitwiseOrEquals, "BitwiseOrEquals" },
-    { "^=", "\\^=", svs::OperatorLexicalToken::Type::BitwiseExclusiveOrEquals, "BitwiseExclusiveOrEquals" },
-    { "<<=", "<<=", svs::OperatorLexicalToken::Type::LogicalLeftShiftEquals, "LogicalLeftShiftEquals" },
-    { ">>=", ">>=", svs::OperatorLexicalToken::Type::LogicalRightShiftEquals, "LogicalRightShiftEquals" },
-    { "<<<=", "<<<=", svs::OperatorLexicalToken::Type::ArithmeticLeftShiftEquals, "ArithmeticLeftShiftEquals" },
-    { ">>>=", ">>>=", svs::OperatorLexicalToken::Type::ArithmeticRightShiftEquals, "ArithmeticRightShiftEquals" },
+    { "=", "=", svs::OperatorLexicalToken::Type::SimpleAssignment, "SimpleAssignment" },
+    { "+=", "\\+=", svs::OperatorLexicalToken::Type::AdditionAssignment, "AdditionAssignment" },
+    { "-=", "-=", svs::OperatorLexicalToken::Type::SubtractionAssignment, "SubtractionAssignment" },
+    { "*=", "\\*=", svs::OperatorLexicalToken::Type::MultiplicationAssignment, "MultiplicationAssignment" },
+    { "/=", "/=", svs::OperatorLexicalToken::Type::DivisionAssignment, "DivisionAssignment" },
+    { "%=", "%=", svs::OperatorLexicalToken::Type::ModuloAssignment, "ModuloAssignment" },
+    { "&=", "&=", svs::OperatorLexicalToken::Type::BitwiseAndAssignment, "BitwiseAndAssignment" },
+    { "|=", "\\|=", svs::OperatorLexicalToken::Type::BitwiseOrAssignment, "BitwiseOrAssignment" },
+    { "^=", "\\^=", svs::OperatorLexicalToken::Type::BitwiseExclusiveOrAssignment, "BitwiseExclusiveOrAssignment" },
+    { "<<=", "<<=", svs::OperatorLexicalToken::Type::LogicalLeftShiftAssignment, "LogicalLeftShiftAssignment" },
+    { ">>=", ">>=", svs::OperatorLexicalToken::Type::LogicalRightShiftAssignment, "LogicalRightShiftAssignment" },
+    { "<<<=", "<<<=", svs::OperatorLexicalToken::Type::ArithmeticLeftShiftAssignment, "ArithmeticLeftShiftAssignment" },
+    { ">>>=", ">>>=", svs::OperatorLexicalToken::Type::ArithmeticRightShiftAssignment, "ArithmeticRightShiftAssignment" },
 
     // Increment and Decrement Operators
     { "++", "\\+\\+", svs::OperatorLexicalToken::Type::Increment, "Increment" },
--- src/lexical_token/OperatorLexicalToken.h
+++ src/lexical_token/OperatorLexicalToken.h
@@ -2,34 +2,131 @@
 #define SVS_OPERATOR_LEXICAL_TOKEN_H_
 
 #include <regex>
+#include <sys/types.h>
 
 #include "LexicalToken.h"
 
 namespace svs
 {
 
+/**
+ * The operator lexical token type.
+ */
 class OperatorLexicalToken : public svs::LexicalToken
 {
 public:
+    /**
+     * The type of operator.
+     */
     enum Type
     {
         // Assignment Operators
-        Equals,
-        PlusEquals,
-        MinusEquals,
-        TimesEquals,
-        DivideEquals,
-        ModuloEquals,
-        BitwiseAndEquals,
-        BitwiseOrEquals,
-        BitwiseExclusiveOrEquals,
-        LogicalLeftShiftEquals,
-        LogicalRightShiftEquals,
-        ArithmeticLeftShiftEquals,
-        ArithmeticRightShiftEquals,
+
+        /**
+         * The simple assignment operator.
+         *
+         * Syntax: '='
+         */
+        SimpleAssignment,
+
+        /**
+         * The addition assignment operator.
+         *
+         * Syntax: '+='
+         */
+        AdditionAssignment,
+
+        /**
+         * The subtraction assignment operator.
+         *
+         * Syntax: '-='
+         */
+        SubtractionAssignment,
+
+        /**
+         * The multiplication assignment operator.
+         *
+         * Syntax: '*='
+         */
+        MultiplicationAssignment,
+
+        /**
+         * The division assignment operator.
+         *
+         * Syntax: '/='
+         */
+        DivisionAssignment,
+
+        /**
+         * The modulo (integer remainder) assignment operator.
+         *
+         * Syntax: '%='
+         */
+        ModuloAssignment,
+
+        /**
+         * The bitwise AND assignment operator.
+         *
+         * Syntax: '&='
+         */
+        BitwiseAndAssignment,
+
+        /**
+         * The bitwise OR assignment operator.
+         *
+         * Syntax: '|='
+         */
+        BitwiseOrAssignment,
+
+        /**
+         * The bitwise exclusive or (XOR) assignment operator.
+         *
+         * Syntax: '^='
+         */
+        BitwiseExclusiveOrAssignment,
+
+        /**
+         * The logical left shift assignment operator.
+         *
+         * Syntax: '<<='
+         */
+        LogicalLeftShiftAssignment,
+
+        /**
+         * The logical right shift assignment operator.
+         *
+         * Syntax: '>>='
+         */
+        LogicalRightShiftAssignment,
+
+        /**
+         * The arithmetic left shift assignment operator.
+         *
+         * Syntax: '<<<='
+         */
+        ArithmeticLeftShiftAssignment,
+
+        /**
+         * The arithmetic right shift assignment operator.
+         *
+         * Syntax: '>>>='
+         */
+        ArithmeticRightShiftAssignment,
 
         // Increment and Decrement Operators
+
+        /**
+         * The increment operator.
+         *
+         * Syntax: '++'
+         */
         Increment,
+
+        /**
+         * The decrement operator.
+         *
+         * Syntax: '--'
+         */
         Decrement,
     };
 
