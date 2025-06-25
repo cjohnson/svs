26f3666d339629a5bb7c339fd72cd72eac8aff02
cjohnson <collin.g.johnson@gmail.com>
2025-06-25 00:49:02 -0400

    Add spec test for escaping line breaks in string literals, test

--- src/parser/sv2017/string_literal_parser.cpp
+++ src/parser/sv2017/string_literal_parser.cpp
@@ -4,18 +4,54 @@ svs::ParseResult<std::string> svs::StringLiteralParser::parse(
     const std::string::const_iterator& begin,
     const std::string::const_iterator& end) const
 {
-    const svs::CharacterParser double_quote_parser('\"');
+    const svs::CharacterParser double_quote_parser{'"'};
+    const svs::CharacterParser backslash_parser{'\\'};
 
-    svs::ParseResult<char> opening_double_quote_result =
+    const svs::ParseResult<char> opening_double_quote_result =
         double_quote_parser.parse(begin, end);
     if (!opening_double_quote_result.succeeded())
     {
         return svs::ParseResult<std::string>::fail();
     }
 
-    return svs::UntilSuccessParser<char>
+    std::stringstream ss;
+    std::string::const_iterator it = opening_double_quote_result.next();
+    while (it != end)
     {
-        double_quote_parser,
-        true,
-    }.parse(opening_double_quote_result.next(), end);
+        svs::ParseResult<char> ending_double_quote_result =
+            double_quote_parser.parse(it, end);
+        if (ending_double_quote_result.succeeded())
+        {
+            return svs::ParseResult<std::string>::succeed(ss.str(), ending_double_quote_result.next());
+        }
+
+        svs::ParseResult<char> backslash_result =
+            backslash_parser.parse(it, end);
+        if (!backslash_result.succeeded())
+        {
+            ss << *it;
+            ++it;
+
+            continue;
+        }
+
+        ++it;
+        if (it == end)
+        {
+            return svs::ParseResult<std::string>::fail();
+        }
+
+        switch (*it)
+        {
+        case '\n':
+            ++it;
+            break;
+        default:
+            ss << '\\';
+            ss << *it;
+            ++it;
+            break;
+        }
+    }
+    return svs::ParseResult<std::string>::fail();
 }
--- test/parser/sv2017/string_literal_parser_tests.cpp
+++ test/parser/sv2017/string_literal_parser_tests.cpp
@@ -27,3 +27,18 @@ TEST(StringLiteralParserTests, MinimalForm)
     EXPECT_EQ(result.value(), "");
     EXPECT_EQ(result.next(), string.end());
 }
+
+TEST(StringLiteralParserTests, SpecTest1)
+{
+    svs::StringLiteralParser parser;
+    std::string string = "\"Humpty Dumpty sat on a wall. \\\nHumpty Dumpty had a great fall.\"";
+
+    svs::ParseResult<std::string> result = parser.parse(
+        string.begin(),
+        string.end());
+
+    ASSERT_TRUE(result.succeeded());
+    EXPECT_EQ(result.value(), "Humpty Dumpty sat on a wall. Humpty Dumpty had a great fall.");
+    EXPECT_EQ(result.next(), string.end());
+}
+
