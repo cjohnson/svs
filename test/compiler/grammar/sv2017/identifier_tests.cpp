4f881c6873ded492154d298a8425fd152cf3eead
Collin Johnson <collj@umich.edu>
2025-10-20 16:23:45 -0400

    Migrate simple identifier lexy tests to flex

--- test/compiler/grammar/sv2017/identifier_tests.cpp
+++ test/compiler/grammar/sv2017/identifier_tests.cpp
@@ -38,27 +38,6 @@ TEST(SV2017SystemTFIdentifierTests, ParsesWithWhitespaceAtEnd)
     EXPECT_PARSE_RESULT<system_tf_identifier>("$display\n", "display");
 }
 
-TEST(SV2017SimpleIdentifierTests, FailsToParseStartingWithDollarSign)
-{
-    EXPECT_PARSE_FAILURE<simple_identifier>("$display");
-}
-
-TEST(SV2017SimpleIdentifierTests, ParsesTestsFromSpec)
-{
-    EXPECT_PARSE_RESULT<simple_identifier>("shiftreg_a", "shiftreg_a");
-    EXPECT_PARSE_RESULT<simple_identifier>("busa_index", "busa_index");
-    EXPECT_PARSE_RESULT<simple_identifier>("error_condition", "error_condition");
-    EXPECT_PARSE_RESULT<simple_identifier>("merge_ab", "merge_ab");
-    EXPECT_PARSE_RESULT<simple_identifier>("_bus3", "_bus3");
-    EXPECT_PARSE_RESULT<simple_identifier>("n$657", "n$657");
-}
-
-TEST(SV2017SimpleIdentifierTests, ParsesAtLeast1024Characters)
-{
-    std::string string(1024, 'a');
-    EXPECT_PARSE_RESULT<simple_identifier>(string, string);
-}
-
 TEST(SV2017EscapedIdentifierTests, FailsToParseEmptyString)
 {
     EXPECT_PARSE_FAILURE<escaped_identifier>("");
--- test/compiler/sv2017/identifier_tests.cpp
+++ test/compiler/sv2017/identifier_tests.cpp
@@ -1,30 +1,74 @@
+#include <vector>
+
 #include <sv2017/scanner.h>
 
 #include <gtest/gtest.h>
 
-TEST(SV2017SimpleIdentifierTests, FailsToParseEmptyString)
-{
-    svs::sv2017::string_scanner s{""};
-
+struct token_info_t {
     yytokentype token;
-    s >> token;
+    YYSTYPE val;
+};
+std::vector<token_info_t> lex_all(const std::string& input) {
+    svs::sv2017::string_scanner scanner{input};
+    std::vector<token_info_t> result;
 
-    EXPECT_FALSE(s);
+    try {
+        yytokentype token;
+        while (scanner >> token) {
+            switch (token) {
+            case T_Identifier:
+                result.push_back(token_info_t{ token, yylval.string_val });
+                break;
+            default:
+                break;
+            }
+        }
+    } catch(const std::runtime_error& e) {
+    }
+    return result;
 }
 
-TEST(SV2017SimpleIdentifierTests, FailsToParseDollarSign)
-{
-    svs::sv2017::string_scanner s{"$"};
-
-    yytokentype token;
-    EXPECT_THROW(s >> token, std::runtime_error);
+TEST(SV2017SimpleIdentifierTests, FailsToParseEmptyString) {
+    std::vector<token_info_t> tokens = lex_all("");
+    EXPECT_TRUE(tokens.empty());
 }
 
-TEST(SV2017SimpleIdentifierTests, FailsToParseStartingWithDigit)
-{
-    svs::sv2017::string_scanner s{"7notident"};
+TEST(SV2017SimpleIdentifierTests, FailsToParseDollarSign) {
+    std::vector<token_info_t> tokens = lex_all("$");
+    EXPECT_TRUE(tokens.empty());
+}
 
-    yytokentype token;
-    EXPECT_THROW(s >> token, std::runtime_error);
+TEST(SV2017SimpleIdentifierTests, FailsToParseStartingWithDigit) {
+    std::vector<token_info_t> tokens = lex_all("7notident");
+    EXPECT_TRUE(tokens.empty());
+}
+
+TEST(SV2017SimpleIdentifierTests, FailsToParseStartingWithDollarSign) {
+    std::vector<token_info_t> tokens = lex_all("$display");
+    EXPECT_TRUE(tokens.empty());
+}
+
+void EXPECT_IDENTIFIER(const std::string& lexer_input, const std::string& expected_value) {
+    std::vector<token_info_t> tokens = lex_all(lexer_input);
+    EXPECT_EQ(tokens.size(), 1);
+
+    const token_info_t& first = tokens.at(0);
+    EXPECT_EQ(first.token, yytokentype::T_Identifier);
+    EXPECT_EQ(*first.val.string_val, expected_value);
+}
+
+TEST(SV2017SimpleIdentifierTests, ParsesTestsFromSpec) {
+    EXPECT_IDENTIFIER("shiftreg_a", "shiftreg_a");
+    EXPECT_IDENTIFIER("busa_index", "busa_index");
+    EXPECT_IDENTIFIER("error_condition", "error_condition");
+    EXPECT_IDENTIFIER("merge_ab", "merge_ab");
+    EXPECT_IDENTIFIER("_bus3", "_bus3");
+    EXPECT_IDENTIFIER("n$657", "n$657");
+}
+
+TEST(SV2017SimpleIdentifierTests, ParsesAtLeast1024Characters)
+{
+    std::string string(1024, 'a');
+    EXPECT_IDENTIFIER(string, string);
 }
 
