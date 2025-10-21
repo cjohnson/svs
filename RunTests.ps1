846f124ff80b024c0a2fe0fc36231ee91013e1ce
Collin Johnson <collin.g.johnson@gmail.com>
2025-10-21 09:57:45 -0400

    Remove old build scrips, use CMake

--- RunTests.ps1
+++
@@ -1,9 +0,0 @@
-# Configure build
-New-Item -Force -ItemType Directory Build 
-cmake . -B Build -G Ninja
-
-# Build
-cmake Build -DCMAKE_BUILD_TYPE=Debug
-cmake --build Build
-
-./build/cpp_test.exe
